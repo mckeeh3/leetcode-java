8bafba6b1e0b4f11e53ef78595afefd4f448b538
Hugh McKee <mckee.hugh@gmail.com>
2016-02-08 04:01:17 -0500

    add ListNode utility class

---
+++ src/main/java/problems/ListNode.java
@@ -0,0 +1,12 @@
+package problems;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int value;
+    ListNode next;
+    ListNode(int x) {
+        value = x;
+    }
+}
