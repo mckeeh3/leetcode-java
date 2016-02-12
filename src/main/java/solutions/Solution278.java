81bad18c9fe915d4b0a8240e646cf3acc63fa5d7
Hugh McKee <mckee.hugh@gmail.com>
2016-02-12 03:40:48 -0500

    Add solution 278

--- src/main/java/solutions/Solution278.java
+++ src/main/java/solutions/Solution278.java
@@ -4,10 +4,10 @@ package solutions;
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version
  * of your product fails the quality check. Since each version is developed based on the previous version, all the
  * versions after a bad version are also bad.
- * <p></p>Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the
- * following ones to be bad.
- * <p></p>You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a
- * function to find the first bad version. You should minimize the number of calls to the API.
+ * <p>Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the
+ * following ones to be bad.</p>
+ * <p>You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a
+ * function to find the first bad version. You should minimize the number of calls to the API.</p>
  *
  * @see <a href="https://leetcode.com/problems/first-bad-version/">278. First Bad Version</a>
  */
