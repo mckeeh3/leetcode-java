aeda93d08176f22a05081b61df0ff9a51262d6f5
Hugh McKee <mckee.hugh@gmail.com>
2016-02-15 19:27:07 -0500

    Add solution 237

--- src/main/java/solutions/Solution237.java
+++ src/main/java/solutions/Solution237.java
@@ -1,7 +1,9 @@
 package solutions;
 
 /**
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
  *
+ * @see <a href="https://leetcode.com/problems/delete-node-in-a-linked-list/">237. Delete Node in a Linked List</a>
  */
 public class Solution237 {
     public void deleteNode(ListNode node) {
